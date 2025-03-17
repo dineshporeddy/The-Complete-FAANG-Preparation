0e418dd0e49bc1ab14debeb63f6eddfbfd0e6712
dineshporeddy <dinesh.poreddy@gmail.com>
2025-03-17 14:13:43 +0530

    Update _049)_Remove-Duplicates-From-Sorted-Array.cpp

--- 1]. DSA + CP/1]. DSA/1]. Data Structures/01]. Array/C++/_049)_Remove-Duplicates-From-Sorted-Array.cpp
+++ 1]. DSA + CP/1]. DSA/1]. Data Structures/01]. Array/C++/_049)_Remove-Duplicates-From-Sorted-Array.cpp
@@ -11,3 +11,18 @@
         }
         return k;
     }
+int main()
+{
+    vector<int> v = {10, 20, 20, 30, 30, 30, 30};
+    int n=7;
+    cout<<"Before Removal Duplicates: ";
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout<<endl<<"Size = "<<n<<endl;
+
+    int Res = removeDuplicates(v);
+    cout<<"After Removal Duplicates: ";
+    for (int i = 0; i < Res; i++)
+        cout << v[i] << " ";
+    cout<<endl<<"Size = "<<Res<<endl;
+}
